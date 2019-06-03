757eeed1bed85262776194239d11d3e860678bcc
Edwin Capel <edwinrc1026@gmail.com>
2019-06-03 23:24:50 +0800

    Updates

---
+++ precache-manifest.3f03fdba8bf9ab55559b38c81ca77372.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64d16890b9a386931d0e7392b14a9286",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53f8bd90acc6b1843bbb",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "faae6684d557a37a8b9c",
+    "url": "/static/css/main.a03d9e24.chunk.css"
+  },
+  {
+    "revision": "53f8bd90acc6b1843bbb",
+    "url": "/static/js/2.8907ca7d.chunk.js"
+  },
+  {
+    "revision": "faae6684d557a37a8b9c",
+    "url": "/static/js/main.3edcfbd3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/edwincapel/edwincapel.github.io/precache-manifest.a2be6b54517de7e8f42304f6b9b7484b.js"
+  "/precache-manifest.3f03fdba8bf9ab55559b38c81ca77372.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/edwincapel/edwincapel.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
